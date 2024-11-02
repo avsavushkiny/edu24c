86c58128adb4d794dee474d15e38230704974d4d
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2024-11-03 01:03:13 +0300

    031124_0103

--- setC/setC.cpp
+++ setC/setC.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int summa(int i, int ii, string text)
-{
-    return i + ii;
-}
+int arr[] = {1,2,3,4,5};  //размер массива указали не явно
+int arr2[3] = {10,20,30}; //размер массива указали явно
+
+char arr3[] = {'h','e','l','l','o'};
 
 int main()
 { 
-    cout << summa(4, 4, "int") << endl;
+    //для arr
+    for (int j = 0; j < (sizeof(arr)/sizeof(arr[0])); j++)
+    {
+        cout << arr[j] << endl;
+    }
+    
+    //для arr2
+    for (int i = 0; i < 3; i++)
+    {
+        cout << arr2[i] << endl;
+    }
+
+    //для arr3 просто применили короткую запись цикла for
+    //для каждого ch из arr3
+    for (char ch : arr3)
+    {
+        cout << ch;
+    }
 
     return 0;
 }
