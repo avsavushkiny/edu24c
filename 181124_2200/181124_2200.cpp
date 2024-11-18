7e3a990fb71d00ffcecddbf9e845dfcdd74a6c3e
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2024-11-19 00:29:23 +0300

    191124_0029

---
+++ 181124_2200/181124_2200.cpp
@@ -0,0 +1,8 @@
+#include "lib2.h"
+
+kernel<2> OS;
+
+int main()
+{
+    OS.runTask();
+}
---
+++ 181124_2200/lib2.h
@@ -0,0 +1,39 @@
+#ifndef LIB2_H
+#define LIB2_H
+
+#include <iostream>
+using namespace std;
+
+void systemFunc() { cout << "system func" << endl; }
+
+template <int8_t _amount>
+class kernel
+{
+public:
+    
+    struct Appl
+    {
+        int num;
+        string text;
+        void (*f)(void);
+    };
+
+    Appl commands[_amount]
+    {
+        {1, "system", systemFunc},
+    };
+
+    void runTask()
+    {
+        for (Appl comm : commands)
+        {
+            comm.f();
+        }
+    }
+
+private:
+    void (*callbacks[_amount])() = {};
+    int num[_amount]; string text[_amount]; 
+};
+
+#endif
