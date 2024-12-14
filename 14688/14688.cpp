f906af26753fcc871374b4fe1237f923f19554ab
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2024-12-14 14:57:04 +0300

    141224

---
+++ 14688/14688.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int x, y, z;
+
+int main()
+{
+    cout << "x" << "y" << "z" << endl;
+    
+    for (x = 0; x <= 1; x++)
+    {
+        for (y = 0; y <= 1; y++)
+        {
+            for (z = 0; z <= 1; z++)
+            {
+                int F = ((x || y) <= (z == x));
+                
+                if (F == 0)
+                {
+                    cout << x << y << z << endl;
+                }
+            }
+        }
+    }
+    
+    return 0;
+}
