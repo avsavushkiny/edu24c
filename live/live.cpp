c6b9c7f86e652dd72d93a52188f535820e6f368f
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2024-12-14 11:36:44 +0300

    141224

--- live/live.cpp
+++ live/live.cpp
@@ -1,23 +1,69 @@
 #include <iostream>
 using namespace std;
 
-struct event
+struct Student
 {
     string name;
-    int data;
-    short school;
+    int groupNumber;
+    double averageGrade;
 };
 
-event newYear9 = {"New Year 25", 221224, 9};
-event newYear12 = {"New Year Super", 251224, 12};
-
-void printEvent(event *p)
+Student MGU[] = 
 {
-    cout << p->name << p->data << p->school << endl;
+    {"Ivan", 104,     3.8},
+    {"Jenia", 104,    4.2},
+    {"Viktoria", 101, 4.0},
+    {"Vitia", 101,    4.7},
+    {"Matvey", 104,   4.8}
+};
+
+void averageGradeAll()
+{
+    double result{};
+    for (int i = 0; i < 5; i++)
+    {
+        result = result + MGU[i].averageGrade;
+    }
+
+    cout << result << endl;
+    cout << "result: " << (result / 5.0) << endl;
+
+    double arr[5];
+
+    for (int j = 0; j < 5; j++)
+    {
+        arr[j] = MGU[j].averageGrade;
+    }
+
+    for(int l = 0; l < 5; l++)
+    {
+        cout << arr[l] << " ";
+    }
+
+    cout << endl;
+
+    double buffer{};
+    for (int y = 0; y < 5; y++)
+    {
+        for (int k = 0; k < 5; k++)
+        {
+            if (arr[k] < arr[k + 1])
+            {
+                buffer = arr[k];
+                arr[k] = arr[k + 1];
+                arr[k + 1] = buffer;
+            }
+        }
+    }
+
+    for(int l = 0; l < 5; l++)
+    {
+        cout << arr[l] << " ";
+    }
 }
 
 int main()
 {
-    printEvent(&newYear9);
+    averageGradeAll();
     return 0;
 }
