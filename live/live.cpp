60278333a0e19064431ff29d3fc4738be79ce266
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2025-01-10 17:52:30 +0300

    100125
    compare

--- live/live.cpp
+++ live/live.cpp
@@ -1,69 +1,52 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct Student
 {
-    string name;
-    int groupNumber;
-    double averageGrade;
+    string fullName; int groupNumber; string faculty;
 };
 
-Student MGU[] = 
+Student allStudentMGU[] =
 {
-    {"Ivan", 104,     3.8},
-    {"Jenia", 104,    4.2},
-    {"Viktoria", 101, 4.0},
-    {"Vitia", 101,    4.7},
-    {"Matvey", 104,   4.8}
+    {"Golovina Viktoriya", 103, "IT Security"},
+    {"Ryabkov Artem", 103, "IT Security"},
+    {"Emelin Egor", 104, "IT Security"},
+    {"Soloncov Matvey", 105, "IT Security"},
+    {"Chadin Ivan", 103, "IT Security"}, 
+    {"Glotov Artem", 105, "Server Security"},
+    {"Artem Glyhov", 106, "IT Security"}
 };
 
-void averageGradeAll()
+void studentPoisk()
 {
-    double result{};
-    for (int i = 0; i < 5; i++)
+    int a;
+    for (;;)
     {
-        result = result + MGU[i].averageGrade;
-    }
-
-    cout << result << endl;
-    cout << "result: " << (result / 5.0) << endl;
-
-    double arr[5];
-
-    for (int j = 0; j < 5; j++)
-    {
-        arr[j] = MGU[j].averageGrade;
-    }
-
-    for(int l = 0; l < 5; l++)
-    {
-        cout << arr[l] << " ";
-    }
-
-    cout << endl;
-
-    double buffer{};
-    for (int y = 0; y < 5; y++)
-    {
-        for (int k = 0; k < 5; k++)
+        string text = "";
+        cin >> text;
+        for (Student st : allStudentMGU)
         {
-            if (arr[k] < arr[k + 1])
+            int aa = st.fullName.compare(text);
+            if (aa > 5)
             {
-                buffer = arr[k];
-                arr[k] = arr[k + 1];
-                arr[k + 1] = buffer;
+                cout << st.fullName << st.groupNumber << st.faculty << endl;
+                break;
             }
+            else cout << "re-enter data" << endl;
         }
-    }
-
-    for(int l = 0; l < 5; l++)
-    {
-        cout << arr[l] << " ";
+        cout << "quit -> 0 or next -> 1?";
+        cin >> a;
+        if (a == 0) {
+            break;
+        }
+        if (a == 1)
+            cout << "Enter name Student" << endl;
     }
 }
 
 int main()
 {
-    averageGradeAll();
-    return 0;
+    studentPoisk(); return 0;
 }
+
