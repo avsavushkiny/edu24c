da089d984228df2e2c1f4e48f78ba275d3fb0bbf
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2025-02-01 14:04:47 +0300

    010225_1404

--- structunionenum/structunionenum.cpp
+++ structunionenum/structunionenum.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class A
+class A //базовый
 {
-public: int f, d;
-private: int k, l;
-};
-
-class B : A
-{
-public: int summa() { return f + d; }
+public: ~A(){}; virtual void summa() = 0;
 private:
 };
+
+class B : public A // производный класс
+{
+public:
+    B(int j, int i) : m_j(j), m_i(i) {}
+    void summa() override { cout << m_j + m_i << endl;}
+private:
+    int m_j, m_i;
+};
+
 
 
 int main()
 {
-    A a; B b; a.d = 3, a.f = 2; 
-    cout << b.summa() << endl;
+    B b(5, 3); b.summa();
+    return 0;
 }
